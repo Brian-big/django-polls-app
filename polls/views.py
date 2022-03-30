dc341e6dd56a83b474f38d9edd8068240a3dfd8a
brianBig <briannbig@gmail.com>
2022-03-30 13:45:53 +0300

    add more views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,17 @@
+from urllib import response
 from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Polls index")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at results for question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
